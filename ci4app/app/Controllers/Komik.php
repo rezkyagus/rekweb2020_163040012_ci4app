09b85a0085e21154d878800baf9500c67c47a4ab
rezkyagus <rezky.163040012@mail.unpas.ac.id>
2020-10-23 18:57:58 +0700

    menambahkan fitur model dan halaman komik

---
+++ ci4app/app/Controllers/Komik.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+
+    public function __construct()
+    {
+        $this->komikModel = new KomikModel();
+    }
+
+    public function index()
+    {
+        $komik = $this->komikModel->findAll();
+
+        $data = [
+            'title' => 'Daftar Komik',
+            'komik' => $komik
+
+        ];
+
+        // $komikModel = new \app\Models\KomikModel();
+        // $komikModel = new KomikModel();
+
+
+
+        return view('komik/index', $data);
+    }
+}
