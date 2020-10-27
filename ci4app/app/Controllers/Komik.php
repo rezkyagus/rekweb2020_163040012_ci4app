71cd25636c5f68ac338bcb965d13f7a7115c98a3
rezkyagus <rezky.163040012@mail.unpas.ac.id>
2020-10-27 09:33:43 +0700

    menambahkan detaik komik

--- ci4app/app/Controllers/Komik.php
+++ ci4app/app/Controllers/Komik.php
@@ -16,11 +16,11 @@ class Komik extends BaseController
 
     public function index()
     {
-        $komik = $this->komikModel->findAll();
+        // $komik = $this->komikModel->findAll();
 
         $data = [
             'title' => 'Daftar Komik',
-            'komik' => $komik
+            'komik' => $this->komikModel->getKomik()
 
         ];
 
@@ -30,5 +30,16 @@ class Komik extends BaseController
 
 
         return view('komik/index', $data);
+    }
+
+    public function detail($slug)
+    {
+        $komik = $this->komikModel->getKomik($slug);
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+
+        return view('komik/detail', $data);
     }
 }
